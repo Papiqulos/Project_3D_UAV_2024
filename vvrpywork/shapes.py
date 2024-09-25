3c9b08cebf71f48112ee7f85e5ea7fb8e88066fc
George Papoutsas <osfpgeopap1925@gmail.com>
2024-09-25 16:23:14 +0300

    Refactor imports in scene.py and shapes.py

--- vvrpywork/shapes.py
+++ vvrpywork/shapes.py
@@ -8,8 +8,8 @@ from math import sin, cos, acos
 from numbers import Number
 import numpy as np
 import open3d as o3d
-import open3d.visualization.gui as gui
-import open3d.visualization.rendering as rendering
+import open3d.visualization.gui as gui # type: ignore
+import open3d.visualization.rendering as rendering # type: ignore
 from pyglet import shapes
 from pyglet.graphics import Group
 import pyglet.gl
